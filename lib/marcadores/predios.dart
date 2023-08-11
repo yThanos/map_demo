d075ea642a825b4682b709907e2321c7e2a16b5d
Vitor Fraporti <vitorfraporti@hotmail.com>
2023-08-11 11:15:41 -0300

    corrigi uma coisinha

--- lib/marcadores/predios.dart
+++ lib/marcadores/predios.dart
@@ -3,40 +3,40 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 Future<Set<Marker>> loadPredios() async{
   Set<Marker> set = {};
   set.add(Marker(
-      markerId: MarkerId("72C"),
-      position: LatLng(-29.722279977714823, -53.71779118905314),
-      infoWindow: InfoWindow(
-          title: "72 C"
-      ),
+    markerId: MarkerId("72C"),
+    position: LatLng(-29.722279977714823, -53.71779118905314),
+    infoWindow: InfoWindow(
+        title: "72 C"
+    ),
     icon: await BitmapDescriptor.fromAssetImage(ImageConfiguration(size: Size(40,40)), "assets/images/72C.png")
   ));
-      set.add( Marker(
-      markerId: MarkerId("72D"),
-      position: LatLng(-29.722518789025543, -53.71772452491716),
-      infoWindow: InfoWindow(
-          title: "72D"
-      ),
-  ));
-          set.add( Marker(
-      markerId: MarkerId("72"),
-      position: LatLng(-29.722739508064972, -53.717666193798166),
-      infoWindow: InfoWindow(
-          title: "72 E"
-      )
+  set.add( Marker(
+    markerId: MarkerId("72D"),
+    position: LatLng(-29.722518789025543, -53.71772452491716),
+    infoWindow: InfoWindow(
+        title: "72D"
+    ),
   ));
   set.add( Marker(
-      markerId: MarkerId("72 F"),
-      position: LatLng(-29.72304591233729, -53.71774163582372),
-      infoWindow: InfoWindow(
-          title: "72 F"
-      )
+    markerId: MarkerId("72E"),
+    position: LatLng(-29.722739508064972, -53.717666193798166),
+    infoWindow: InfoWindow(
+        title: "72 E"
+    )
   ));
   set.add( Marker(
-      markerId: MarkerId("72 G"),
-      position: LatLng(-29.723250183135367, -53.7176487101325),
-      infoWindow: InfoWindow(
-          title: "72 G"
-      )
+    markerId: MarkerId("72F"),
+    position: LatLng(-29.72304591233729, -53.71774163582372),
+    infoWindow: InfoWindow(
+        title: "72 F"
+    )
+  ));
+  set.add( Marker(
+    markerId: MarkerId("72G"),
+    position: LatLng(-29.723250183135367, -53.7176487101325),
+    infoWindow: InfoWindow(
+        title: "72 G"
+    )
   ));
   return set;
 }
